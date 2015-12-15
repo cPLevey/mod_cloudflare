7ffcb5f45b4cbe68caf9056a3a7830f0dc6414ca
cPlevey <cplevey@gmail.com>
2015-12-15 04:48:05 -0600

    --no-check-certificate

--- EasyApache/installer.sh
+++ EasyApache/installer.sh
@@ -4,18 +4,18 @@ CPANEL_DIR="/var/cpanel/easy/apache/custom_opt_mods"
 INSTALL_DIR="$CPANEL_DIR/Cpanel/Easy"
 
 if [ ! -d "$CPANEL_DIR" ]; then
-	echo "$CPANEL_DIR not found. This script should only be used on servers that have cPanel installed."
-	exit 1
+        echo "$CPANEL_DIR not found. This script should only be used on servers that have cPanel installed."
+        exit 1
 fi
 
 if [ ! -d "$INSTALL_DIR" ]; then
-	mkdir -p "$INSTALL_DIR"
+        mkdir -p "$INSTALL_DIR"
 fi
 
 # Download & tar mod_cloudflare.c
-mkdir mod_cloudflare && cd mod_cloudflare && wget "https://raw.githubusercontent.com/cloudflare/mod_cloudflare/master/mod_cloudflare.c" && cd ..
+mkdir mod_cloudflare && cd mod_cloudflare && wget --no-check-certificate "https://raw.githubusercontent.com/cloudflare/mod_cloudflare/master/mod_cloudflare.c" && cd ..
 tar -cvzf $INSTALL_DIR/ModCloudflare.pm.tar.gz mod_cloudflare/mod_cloudflare.c
 # Download ModCloudflare.pm into cPanel directory
-wget -O $INSTALL_DIR/ModCloudflare.pm https://raw.githubusercontent.com/cloudflare/mod_cloudflare/master/EasyApache/ModCloudflare.pm
+wget --no-check-certificate -O $INSTALL_DIR/ModCloudflare.pm https://raw.githubusercontent.com/cloudflare/mod_cloudflare/master/EasyApache/ModCloudflare.pm
 # Remove leftover files
 rm -rf mod_cloudflare/
